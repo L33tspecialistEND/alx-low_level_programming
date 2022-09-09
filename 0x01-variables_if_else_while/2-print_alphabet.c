9b0aa274d16d1ba239a0b942ac62e66dd430ec95
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-09-09 06:28:18 -0700

    Add 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio>
+/**
+ * main - Prints the alphabet
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alphabet;
+	alphabet = "abcdefghijklmnopqrstuvwxyz";
+	
+	putchar("%c\n", alphabet);
+
+	return (0);
+}

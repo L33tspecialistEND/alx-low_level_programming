bca98f9010aab8f677c5056991499c6a6bb8e1bc
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-09-13 10:26:54 -0700

    Edit 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,11 +2,9 @@
 #include "main.h"
 
 /**
- * _putchar - Prints character
  * print_alphabet - Prints alphabet in lower case
  *
  * Description: Prints letters a to z in lower case format
- * Return: 0
  */
 void print_alphabet(void)
 {

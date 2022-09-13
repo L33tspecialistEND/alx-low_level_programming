1f1ba611a272e522abdb34cac1b6c2880603c882
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-09-13 07:28:40 -0700

    Edit main.h header file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,16 +8,7 @@
  */
 int main(void)
 {
-	char letter;
-
-	void print_alphabet(void);
-	{
-		for(letter = 'a'; letter <= 'z'; letter++)
-		{
-			_putchar(letter);
-		}
-		_putchar('\n');
-	}
+	print_alphabet();
 
 	return (0);
 }

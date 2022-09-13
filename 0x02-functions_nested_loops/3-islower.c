b62909ca05101cb2b3a3ffd66099e7f0184be951
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-09-13 11:39:55 -0700

    Add 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - Checks if a character is lowercase
+ *
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c<= 12)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+	return (0);
+}

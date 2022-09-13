7237f09b0ad4e558bacaf48afe3ef7d4817522f5
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-09-13 09:34:41 -0700

    Edit 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include "main.h"
 
 /**
@@ -10,4 +11,19 @@ int main(void)
 	print_alphabet();
 
 	return (0);
+}
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+void print_alphabet(void)
+{
+	char letter;
+
+	for(letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
 }

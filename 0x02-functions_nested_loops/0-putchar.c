527c653161d7f5e151805f5e6da908a6b2d919a3
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-09-14 03:08:10 -0700

    Edit 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,14 @@
  */
 int main(void)
 {
-	char str[] = {_, p, u, t, c, h, a, r};
-	int count;
-
-	for(count = 0; count <= 7; count++)
-	{
-		_putchar(str[count]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);

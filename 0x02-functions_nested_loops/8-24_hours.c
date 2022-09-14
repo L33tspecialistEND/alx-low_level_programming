764fde36c8ed444d16f2acd90f61add269c51845
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-09-14 06:44:03 -0700

    Edit 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * jack_bauer - Prints all the minutes of the day
+ * @i: Hour of time to be printed
+ * @j: Minutes of time to be printed
+ */
 void jack_bauer(void)
 {
 	int i, j;
 
 	for (i = 0; i <= 23; i++)
 	{
+		printf("%02d", i);
 		while (i <= 23)
 		{
+			_putchar(':');
 			for (j = 0; j <= 59;)
 			{
-				printf("%02d:%02d", i, j);
+				printf("%02d\n", j);
 			}
 		}
 	}	

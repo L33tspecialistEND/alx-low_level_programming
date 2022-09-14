85cfa13b8ef3b38a41a370f3802a0167b499fa22
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-09-14 03:48:25 -0700

    Add 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,5 +2,18 @@
 
 void jack_bauer(void)
 {
-	
+	int i, j;
+
+	for (i = 0; i <= 23; i++)
+	{
+		while (i <= 23)
+		{
+			for (j = 0; j <= 59;)
+			{
+				_putchar(i);
+				_putchar(':');
+				_putchar(j);
+			}
+		}
+	}	
 }

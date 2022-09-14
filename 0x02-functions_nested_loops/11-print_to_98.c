4793e49e4759b5e16ebdc1b2ffd70581eddcf3f7
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-09-14 08:35:17 -0700

    Edit 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,18 +7,20 @@
  */
 void print_to_98(int n)
 {
-	if (n >= 98)
+	if (n > 98)
 	{
-		for (; n >= 98; n--)
+		for (; n > 98; n--)
 		{
 			printf("%d, ", n);
 		}
+		printf("%d\n", n = 98);
 	}
-	else if (n <= 98)
+	else if (n < 98)
 	{
-		for (; n <= 98; n++)
+		for (; n < 98; n++)
 		{
 			printf("%d, ", n);
 		}
+		printf("%d\n", n = 98);
 	}
 }

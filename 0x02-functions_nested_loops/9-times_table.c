bc02ad8ef99c0bcd9a0572fd15e3ecacf75a359a
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-09-14 07:45:23 -0700

    Edit 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,11 +8,12 @@ void times_table(void)
 {
 	int i, j, result;
 
-	result = i * j;
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
+			result = i * j;
+
 			if (result <= 9)
 			{
 				printf("%d,  ", result);

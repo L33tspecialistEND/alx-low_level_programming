9e5e0b36ecece035495fe798b1cb231b352039e8
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-09-15 00:30:29 -0700

    Add 0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Tests numbers if positive or negative
+ * @i: Number to test
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}

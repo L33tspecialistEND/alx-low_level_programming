994175cc2608f0975c30dd1294c70ec23ad2346a
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-09-15 12:35:35 -0700

    Add 1-_isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks for a number between 0 and 9
+ * @c: Number to pass to the function
+ *
+ * Return: 1 or 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

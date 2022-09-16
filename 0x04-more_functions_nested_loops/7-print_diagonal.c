2f066997c76b778d878338be2b03dcaa104138a7
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-09-16 06:30:01 -0700

    Edit _putchar(n)

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,10 +10,15 @@ void print_diagonal(int n)
 
 	for (i = 0; i < n; i++)
 	{
-		for (j = 1; j < n; j++)
+		for (j = 1; j < i; j++)
 		{
 			_putchar(' ');
 		}
-		_putchar(92'\n');
+		_putchar(92);
+		if (i < (n - 1))
+		{
+			_putchar('\n');
+		}
 	}
+	_putchar('\n');
 }

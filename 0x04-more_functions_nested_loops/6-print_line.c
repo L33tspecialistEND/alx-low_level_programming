d1dd23d820081638cc46826db57581871691bdf3
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-09-16 05:04:11 -0700

    Add 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,16 +2,17 @@
 
 /**
  * print_line - Prints underscore character n times
+ * @n: Parameter to pass to loop in function
  */
 void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
+	if (n <= 48)
 	{
 		_putchar('\n');
 	}
-	for (i = 1; i <= n; i++)
+	for (i = 49; i == n; i++)
 	{
 		_putchar('_');
 	}

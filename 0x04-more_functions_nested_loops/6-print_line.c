4b630607f8772899f4d60a258d5c83058a28ec5b
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-09-16 05:14:51 -0700

    Edit 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,9 +1,15 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
  * print_line - Prints underscore character n times
  * @n: Parameter to pass to loop in function
  */
+int main(void)
+{
+	print_line(3);
+	return (0);
+}
 void print_line(int n)
 {
 	int i;
@@ -14,7 +20,11 @@ void print_line(int n)
 	}
 	for (i = 49; i == n; i++)
 	{
-		_putchar('_');
+		_putchar(95);
 	}
 	_putchar('\n');
+}
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }

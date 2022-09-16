6defa9eb9c77def9324f9ed6f254c98f3f7a6507
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-09-16 04:51:00 -0700

    Edit 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,11 @@ void more_numbers(void)
 		i++;
 		for (j = 48; j <= 62; j++)
 		{
-			_putchar(j);
+			if (j >= 58)
+			{
+				_putchar(j / 58);
+			}
+			_putchar(j % 48);
 		}
 		_putchar('\n');
 	}

b509428b0f0134ea637933700689f67fc028bcc1
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-09-16 06:20:31 -0700

    Edit 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,14 +6,14 @@
  */
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar(92'\n');
-		for (i = 1; i < n; i++)
+		for (j = 1; j < n; j++)
 		{
 			_putchar(' ');
 		}
+		_putchar(92'\n');
 	}
 }

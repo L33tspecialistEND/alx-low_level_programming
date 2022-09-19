d3ae2e2e6414ee2fb31ee165019f04b88eeb6c1d
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-09-19 07:53:12 -0700

    Add 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse
+ * @s: String to be printed
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	for (len = 0; *(s + len) != '\0'; len++)
+	{
+	}
+	for (; ; len--)
+	{
+		_putchar(*(s + len));
+	}
+	_putchar('\n');
+}

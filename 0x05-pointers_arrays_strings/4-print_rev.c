2aee787cd2aed635a536aa15fab5e401b597f3f6
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-09-20 06:27:48 -0700

    Edit 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,14 +7,17 @@
 void print_rev(char *s)
 {
 	int len;
-
-	for (len = 0; *(s + len) != '\0'; len++)
+	
+	len = 0;
+	while (len >= 0)
 	{
+		if (s[len] == '\0')
+			break;
+		len++
 	}
-
 	for (; len >= 0; len--)
 	{
-		_putchar(*(s + len));
+		_putchar(s[len]);
 	}
 	_putchar('\n');
 }

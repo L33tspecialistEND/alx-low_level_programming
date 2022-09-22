476be1053eea6015d0034d48965dd82abed990a0
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-09-22 04:00:12 -0700

    Edit 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,10 +10,30 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int a, b;
+	int a, b, c;
 
-	for (a = 0, b = 0; b <= n; a++, b++)
-		dest[a] = src[b];
+	a = 0;
+	while (a >= 0)
+	{
+		if (src[a + 1] == '\0')
+			break;
+		a++;
+	}
+
+	if (n <= a)
+	{
+		for (b = 0, c = 0; c <= n; b++, c++)
+			dest[b] = src[c];
+	}
+	else if (n > a)
+	{
+		for (b = 0, c = 0; c <= a; b++, c++)
+		{
+			dest[b] = src[c];
+		}
+		for (; (a + 1) <= n; a++)
+			dest[a + 1] = '\0';
+	}
 
 	return (dest);
 }

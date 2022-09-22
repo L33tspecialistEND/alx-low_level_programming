4b4ecf6e5053ef34cb281aaeb7d09ada1b214d85
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-09-22 02:44:05 -0700

    Add 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates n characters from source to destination
+ * @dest: Destination to concatenate to
+ * @src: Source to concatenate from
+ * @n: Number of characters to concatenate
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, b;
+
+	a = 0;
+	while (a >= 0)
+	{
+		if (dest[a + 1] == '\0')
+			break;
+		a++;
+	}
+
+	for (b = 0; b <= n; a++, b++)
+		dest[a + 1] = src[b];
+
+	return (dest);
+}

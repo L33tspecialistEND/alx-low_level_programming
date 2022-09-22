0609b4d517ecc561d272abb4af4660c883c81e10
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-09-22 06:04:47 -0700

    Add 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, j;
+
+	i = 0;
+	while (i >= 0)
+	{
+		if (s1[i] < s2[i] || s1[i] > s2[i])
+		{
+			j = s1[i] - s2[i];
+
+			return (j);
+		}
+		else if (s1[i] == '\0' && s2[i] == '\0')
+			return (0);
+		i++;
+	}
+}

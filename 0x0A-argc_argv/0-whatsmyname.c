364d3df8b59b1afdf3d7a1e14a03f063723363f5
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-09-30 04:54:12 -0700

    Add 0-whatsmyname.c and main.h

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Prints the name of the file
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	_putchar(argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This project is on the arguments of the main function, argc and arv

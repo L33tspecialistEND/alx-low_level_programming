0c53ef773ea75c7c877eaf7b5486fedb2f80ae12
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-10-04 03:28:41 -0700

    Edit 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -5,23 +5,20 @@
  * alloc_grid - Creates a 2D array
  * @width: Array width
  * @height: Array height
- *
  * Return: NULL or grid
  */
 int **alloc_grid(int width, int height)
 {
-	int (*grid)[width][height];
+	int *grid, i;
 
-	grid = malloc(sizeof(*grid));
+	grid = malloc((width * height) * sizeof(int));
 	if (width == 0 || width < 0 || height == 0 || height < 0)
 		return (NULL);
-	if ((*grid) == NULL)
+	if (grid == NULL)
 		return (NULL);
 
-	for (i = 0; i < width; i++)
-	{
-		for (j = 0; j < height; j++)
-			grid[i][j] = 0;
-	}
+	for (i = 0; i < (width * height); i++)
+		grid[i] = 0;
+
 	return (grid);
 }

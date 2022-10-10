4b6ee91ca9811225bf59847c5bfd4b7fb7dacc05
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-10-10 06:45:56 -0700

    Edit dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
-typedef struct dog
+/**
+ * struct dog - Details of the dog
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ */
+struct dog
 {
 	char *name;
 	float age;

0dfecd5b53f3f9e8873b3d093cc27b236cf07a34
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-10-10 08:55:09 -0700

    Add 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog- Frees memory allocated to struct dog
+ * @d: Pointer to struct
+ */
+void free_dog(dog_t *d)
+{
+	free(dog_t);
+}

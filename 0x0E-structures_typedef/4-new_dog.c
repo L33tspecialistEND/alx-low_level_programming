3e68290325fa342c37daf591309587e132905f98
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-10-11 04:47:04 -0700

    Edit 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,14 +4,28 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *Dog;
+	unsigned int i, j, k;
+	dog_t *ndog;
 
-	Dog = malloc(sizeof(dog_t));
-	if (Dog == NULL)
+	if (name == NULL || owner == NULL)
 		return (NULL);
-	(*Dog).name = name;
-	Dog->age = age;
-	(*Dog).owner = owner;
-
-	return (Dog);
+	ndog = malloc(sizeof(dog_t));
+	if (ndog == NULL)
+		return (NULL);
+	for (j = 0; name[j]; j++)
+		;
+	for (k = 0; owner[k]; k++)
+		;
+	ndog->name = malloc(j * sizeof(char));
+	if (ndog->name == NULL)
+		return (NULL);
+	for (i = 0; i < j; i++)
+		ndog->name[i] = name[i];
+	ndog->age = age;
+	ndog-owner = malloc(k * sizeof(char));
+	if (ndog->owner == NULL)
+		return (NULL);
+	for (i = 0; i < k; i++)
+		ndog->owner[i] = owner[i];
+	return (ndog);
 }

a7ac08780f1b0f5b2ed2cc9b0e9757e12ad68fd4
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-10-12 07:21:05 -0700

    Change type of i

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
@@ -10,9 +8,9 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	size_t i;
 
-	if (array != NULL && size != NULL && action != NULL)
+	if (array != NULL && action != NULL)
 	{
 		for (i = 0; i < size; i++)
 			action(array[i]);

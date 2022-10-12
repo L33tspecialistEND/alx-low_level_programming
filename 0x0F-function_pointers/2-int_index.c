2879915383b84174af7eff6a0dfd44db0f926e48
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-10-12 08:12:37 -0700

    Edit 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,9 +14,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-		return (-1);
-	if (array && cmp)
+	if (size > 0 && array && cmp)
 	{
 		for (i = 0; i < size; i++)
 		{
@@ -26,4 +24,6 @@ int int_index(int *array, int size, int (*cmp)(int))
 		if (i == size)
 			return (-1);
 	}
+
+	return (-1);
 }

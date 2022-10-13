1cb689256fa78b19cc63f4ca21cf776ecdac6619
Ebuka Eric Ntiaju <ntiajubukason@gmail.com>
2022-10-13 09:47:20 -0700

    Edit 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,13 +9,13 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	int sum = 0;
+	unsigned int i;
+	if (n == 0)
+		return (0);
 	va_list ap;
 
 	va_start(ap, n);
-	unsigned int i;
-	int sum;
-
-	sum = 0;
 	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
 	va_end(ap);
